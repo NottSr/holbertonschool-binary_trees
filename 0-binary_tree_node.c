7ece4a576487c0be5ab5714739563cb4c1ca0e84
NottSr <leonardo.br4@hotmail.com>
2022-07-26 00:38:09 -0500

    Task 0 and 1 done

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,9 +5,20 @@
  * @parent: Pointer to the parent node of the node to create
  * @value: Value to put in the new node
  *
- * Return - Pointer to the new node, or NULL on failure
+ * Return: Pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	return (NULL);
+	binary_tree_t *new_node = NULL;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /* Structures used */
 
@@ -33,6 +35,7 @@ typedef struct binary_tree_s heap_t;
 
 /* Prototypes used */
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
