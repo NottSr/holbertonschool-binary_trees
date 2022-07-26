841c9f22c105d8c30ecc820d3847702e0a094eaf
NottSr <leonardo.br4@hotmail.com>
2022-07-26 11:38:28 -0500

    Task 3 and 4 done, corrections made

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - Function that inserts a node as
+ * binary_tree_insert_right - Function that inserts a node as
  * the right-child of another node
  *
  * @parent: Pointer to the node to insert the right-child in
