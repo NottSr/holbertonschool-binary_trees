4e04c141e98cdfbd1b2f7eef85e1ec5ba0f1a17d
NottSr <leonardo.br4@hotmail.com>
2022-07-27 18:50:23 -0500

    Fixing code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,7 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int bal_int = 0, bar_int = 0;
 
-	if (tree == NULL)
+	if (tree == NULL || (!tree->left && !tree->right))
 		return (0);
 
 	if (tree->left)
