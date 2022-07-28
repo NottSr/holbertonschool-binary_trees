38b92e65e28c7c4744dc494fa93f1c7472055c46
NottSr <leonardo.br4@hotmail.com>
2022-07-27 19:33:50 -0500

    Task 16 done

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,47 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_height - Function that measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the height.
+ *
+ * Return: Tree height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left = 0, h_right = 0;
+
+	if (tree == NULL || (!tree->left && !tree->right))
+		return (0);
+
+	h_left = binary_tree_height(tree->left);
+	h_right = binary_tree_height(tree->right);
+
+	return (h_left > h_right ? h_left + 1 : h_right + 1);
+}
+/**
+ * binary_tree_balance - Function that measures the balance factor of a
+ * binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: Balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int bal = 0, bar = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		bal = binary_tree_height(tree->left) + 1;
+
+	if (tree->right)
+		bar = binary_tree_height(tree->right) + 1;
+
+	return (bal - bar);
+}
+/**
  * binary_tree_is_perfect - Function that checks if a binary tree is perfect
  *
  * @tree: Pointer to the root node of the tree to check
@@ -8,22 +50,18 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int last_nl = 0, last_nr = 0;
-
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (0);
-
-	if (tree->left || tree->right)
+	if (binary_tree_balance(tree) == 0)
 	{
-		last_nl = (binary_tree_is_perfect(tree->left) + 1);
-		last_nr = (binary_tree_is_perfect(tree->right) + 1);
-	}
+		if (binary_tree_height(tree->left) == 0 &&
+			binary_tree_height(tree->right) == 0)
+			return (1);
 
-	if (last_nl == last_nr)
-		return (1);
+		return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
+	}
 
 	return (0);
 }
